c4698b761b23185803c63b38f8325e3813b75773
fatemeh lohrasbi <lohrasbi.developer@gmail.com>
2023-06-29 12:52:15 +0330

    expense list table added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import ExpenseList from './components/ExpenseList';
 import './App.css';
 
 function App() {
+  const expenses = [
+    {id:1, description: 'aaa', amount: 10, category: 'Utilities'},
+    {id:2, description: 'bbb', amount: 10, category: 'Utilities'},
+    {id:3, description: 'ccc', amount: 10, category: 'Utilities'},
+    {id:4, description: 'ddd', amount: 10, category: 'Utilities'},
+  ]
+
+  // const onDeleteHandler = (id) => {}
+
   return (
     <>
-     
+     <ExpenseList expenses={expenses} onDelete={(id) => console.log('Delete', id)} />
     </>
   )
 }
